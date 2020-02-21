2283aa6858a7ea16d4b34f6f0a93dbe8543aa44b
JC <11434205+dijonkitchen@users.noreply.github.com>
2020-02-21 08:15:05 -0800

    git: include native prompt for zsh as well
    File supports both bash and zsh: https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh#L1

--- Formula/git.rb
+++ Formula/git.rb
@@ -125,6 +125,7 @@ class Git < Formula
     bash_completion.install "contrib/completion/git-completion.bash"
     bash_completion.install "contrib/completion/git-prompt.sh"
     zsh_completion.install "contrib/completion/git-completion.zsh" => "_git"
+    zsh_completion.install "contrib/completion/git-prompt.sh"
     cp "#{bash_completion}/git-completion.bash", zsh_completion
 
     elisp.install Dir["contrib/emacs/*.el"]
